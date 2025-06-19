2e511387c6c18998bc7f629bcb8842729d76920f
horaceo1b <horaceken0@gmail.com>
2025-06-20 00:12:49 +0300

    initial commit

--- src/02-advanced-debug.js
+++ src/02-advanced-debug.js
@@ -24,5 +24,97 @@ function goodPractices() {
   }
 }
 
-// then, call the function so it runs!
+
 goodPractices()
+
+function numPointsScored(playerName) {
+  const game = gameObject();
+
+  for (let team in game) {
+    const players = game[team].players;
+    if (players[playerName]) {
+      return players[playerName].points;
+    }
+  }
+
+  return null; 
+}
+
+function shoeSize(playerName) {
+  const game = gameObject();
+
+  for (let team in game) {
+    const players = game[team].players;
+    if (players[playerName]) {
+      return players[playerName].shoe;
+    }
+  }
+
+  return null;
+}
+
+function teamColors(teamName) {
+  const game = gameObject();
+
+  for (let team in game) {
+    if (game[team].teamName === teamName) {
+      return game[team].colors;
+    }
+  }
+
+  return null;
+}
+
+function teamNames() {
+  const game = gameObject();
+  return [game.home.teamName, game.away.teamName];
+}
+
+function playerNumbers(teamName) {
+  const game = gameObject();
+
+  for (let team in game) {
+    if (game[team].teamName === teamName) {
+      return Object.values(game[team].players).map(player => player.number);
+    }
+  }
+
+  return [];
+}
+
+function playerStats(playerName) {
+  const game = gameObject();
+
+  for (let team in game) {
+    const players = game[team].players;
+    if (players[playerName]) {
+      return players[playerName];
+    }
+  }
+
+  return null;
+}
+
+function bigShoeRebounds() {
+  const game = gameObject();
+
+  let largestShoeSize = 0;
+  let rebounds = 0;
+
+  for (let teamKey in game) {
+    const players = game[teamKey].players;
+
+    for (let playerName in players) {
+      const player = players[playerName];
+
+      if (player.shoe > largestShoeSize) {
+        largestShoeSize = player.shoe;
+        rebounds = player.rebounds;
+      }
+    }
+  }
+
+  return rebounds;
+}
+
+
